37ae298c55092bb116ba904a58ed5cea6b47eef2
Mirko Grönroos <hermo@dmz.fi>
2015-12-02 11:05:55 +0200

    v2.1.1

--- Formula/npmi.rb
+++ Formula/npmi.rb
@@ -1,8 +1,8 @@
 class Npmi < Formula
   desc "Locally caching 'npm install'"
-  version "2.1.0"
-  url "https://github.com/hermo/npmi/archive/v2.1.0.tar.gz"
-  sha1 "5a83557d97279b6f651b8563a3e1466bca098c0e"
+  version "2.1.1"
+  url "https://github.com/hermo/npmi/archive/v2.1.1.tar.gz"
+  sha256 "01041e738593975b5443fc720f7333236e38f6e72b5eeff814516779e5b4c98d"
   homepage "https://github.com/hermo/npmi"
 
   def install
