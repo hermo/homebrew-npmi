51f99c912a4f9eee25b26901c001778b87e18dde
Mirko Grönroos <hermo@dmz.fi>
2016-01-12 21:23:25 +0200

    Fix hash

--- Formula/npmi.rb
+++ Formula/npmi.rb
@@ -2,7 +2,7 @@ class Npmi < Formula
   desc "Locally caching 'npm install'"
   homepage "https://github.com/hermo/npmi"
   url "https://github.com/hermo/npmi/archive/v4.1.0.tar.gz"
-  sha256 "b215f014559facc84dfc7d5eaf4d10f6f279a440099ac4ce2cd43f7b9ed81a64"
+  sha256 "1a8c9faecf83f4a12e4fd478d934c4f937276bca7faaeb55e56b330e89f1b890"
 
   def install
     bin.install "npmi"
