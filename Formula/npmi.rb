5f84847e6db03a1d76d4551fc6a0dbf813c155b3
Mirko Grönroos <hermo@dmz.fi>
2016-01-12 21:20:28 +0200

    Update to 4.1.0

--- Formula/npmi.rb
+++ Formula/npmi.rb
@@ -1,8 +1,8 @@
 class Npmi < Formula
   desc "Locally caching 'npm install'"
   homepage "https://github.com/hermo/npmi"
-  url "https://github.com/hermo/npmi/archive/v4.0.0.tar.gz"
-  sha256 "384c199fa2967461624b3875d1df7ae7b64053d82d267db388469f3f07206a36"
+  url "https://github.com/hermo/npmi/archive/v4.1.0.tar.gz"
+  sha256 "b215f014559facc84dfc7d5eaf4d10f6f279a440099ac4ce2cd43f7b9ed81a64"
 
   def install
     bin.install "npmi"
